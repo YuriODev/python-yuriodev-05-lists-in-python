75f6f1f13a313f5f6b9d16ca0916a240a7865aaa
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-05-25 19:24:27 +0100

    Update example_1.py

--- examples/example_1.py
+++ examples/example_1.py
@@ -8,12 +8,12 @@ numbers = list(map(int, input_sequence.split()))
 if len(numbers) >= 2:
     # Extract the last two elements from the numbers list
     last_two = numbers[-2:]
-    
+
     # Remove the last two elements from the numbers list
     remaining_numbers = numbers[:-2]
-    
+
     # Create a new list by concatenating the last two elements at the beginning of the remaining_numbers list
     new_order = last_two + remaining_numbers
-    
+
     # Convert each element of the new_order list to a string and join them with a space separator
     print(" ".join(map(str, new_order)))
