429a883f72e135d91a132b8be5d824f0b43ad599
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-05-25 21:20:19 +0100

    Update example_13.py

--- examples/example_13.py
+++ examples/example_13.py
@@ -1,10 +1,23 @@
-input_text = input("Enter a line of text: ")
-words = input_text.split()
+# Read the input data
+input_data = input("Enter a sequence of integers separated by spaces: ")
 
-# Count the number of unique words
-# The set() function removes duplicates from the list
-# The len() function returns the number of elements in the set
-unique_words = len(set(words))
+# Convert the input data to a list of integers
+numbers = list(map(int, input_data.split()))
 
-# Print the result
-print(unique_words)
+# Find the smallest number
+min_number = min(numbers)
+
+# Find the largest number
+max_number = max(numbers)
+
+# Find the number of elements
+count_numbers = len(numbers)
+
+# Find the average value
+average_value = sum(numbers) / count_numbers
+
+# Print the results
+print(min_number)
+print(max_number)
+print(count_numbers)
+print(average_value)
