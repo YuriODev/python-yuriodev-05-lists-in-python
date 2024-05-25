2b45da3efa08b8907f8ae275acc78594bfbacfd3
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-05-25 21:20:34 +0100

    Update example_18.py

--- examples/example_18.py
+++ examples/example_18.py
@@ -1,14 +1,8 @@
-# Prompt the user to enter numbers separated by spaces
-input_numbers = input("Enter numbers separated by spaces: ")
+# Input data
+url = input("Enter a URL: ")
 
-# Split the input string into a list of individual numbers and convert them to integers
-numbers = list(map(int, input_numbers.split()))
+# Extract the resource name from the URL
+resource_name = url.split("/")[-1]
 
-# Prompt the user to enter an integer value for n
-n = int(input("Enter an integer n: "))
-
-# Check if n exceeds all elements in the list by comparing it to the maximum value in the list
-exceeds_all = n > max(numbers)
-
-# Print the result indicating whether n exceeds all elements in the list
-print(exceeds_all)
+# Print the result
+print(resource_name)
