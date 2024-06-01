57e1b803dc697bd258f0a88965e8f947d419fa65
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-06-01 16:05:43 +0100

    Create solution_2.py

--- solutions/solution_2.py
+++ solutions/solution_2.py
@@ -1,5 +1,19 @@
-# Read the input
-n = int(input())
+# Solution to Exercise 2
 
-# Calculate and print the next even number
-print(n + 2 - n % 2)
+# Prompt the user to enter names of world languages separated by spaces
+input_data = input("Enter names of world languages separated by spaces: ")
+
+# Convert the input string into a list of languages
+languages = input_data.split()
+
+# Sort the list of languages alphabetically
+languages.sort()
+
+# Reverse the list of languages
+reversed_languages = languages[::-1]
+
+# Solution using sorted function
+reversed_languages = sorted(languages)[::-1]
+
+# Print the reversed list of languages separated by spaces
+print(" ".join(reversed_languages))
