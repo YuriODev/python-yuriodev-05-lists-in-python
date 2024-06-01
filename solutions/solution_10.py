3ab068d9d8ef1ae611e81fccd73a675a9f30a524
allwak <yurii.oksamytnyi@yuriodev.co.uk>
2024-06-01 16:05:43 +0100

    Create solution_10.py

--- solutions/solution_10.py
+++ solutions/solution_10.py
@@ -1,8 +1,16 @@
-# Read the input
-a = float(input())
+# Solution to Exercise 10
 
-# Calculate the angle
-angle = (a % 30) * 12
+# Prompt the user to enter a list of integers separated by spaces
+input_data = input("Enter a list of integers separated by spaces: ")
 
-# Print the angle
-print(angle)
+# Convert the input string into a list of integers
+numbers = list(map(int, input_data.split()))
+
+# Prompt the user to enter an integer n
+n = int(input("Enter an integer: "))
+
+# Count the occurrences of n in the list
+count = numbers.count(n)
+
+# Print the count of occurrences of n
+print(count)
